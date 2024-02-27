f5ebebde75e1c785a4cc857e75f021a320dd4a42
lucas santana <lucasdeoliveirasantana874@gmail.com>
2024-02-27 20:07:44 -0300

    componente ordenacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import BarraDePesquisa from "./components/BarraDePesquisa";
+import Filtro from "./components/Filtro";
+import Ordenacao from "./components/Ordenacao";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="container">
       <Sidebar />
-      <BarraDePesquisa />
+      <div>
+        <BarraDePesquisa />
+        <Filtro />
+        <Ordenacao />
+      </div>
     </div>
   );
 }
